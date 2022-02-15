211739266c9e043e25fbc1a856cfa6767dd22955
Luann Ebert <ltebert@comcast.net>
2022-02-15 01:31:10 +0000

    Project submission

--- p1/README.md
+++ p1/README.md
@@ -5,6 +5,16 @@
 ## Outside resources
 I referred to the following pages in the PHP docs:
 + <https://www.php.net/manual/en/function.array-reverse.php>
++ <https://www.php.net/manual/en/function.preg-replace.php>
++ <https://www.php.net/manual/en/function.chr.php>
++ <https://www.php.net/manual/en/function.ord.php>
+
+And a few other sources:
++ <https://www.w3schools.com/php/func_array_range.asp>
++ <https://www.geeksforgeeks.org/how-to-remove-non-alphanumeric-characters-in-php/>
++ <http://www.hackingwithphp.com/4/7/3/converting-to-and-from-ascii>
++ Table of ascii characters: https://www.man7.org/linux/man-pages/man7/ascii.7.html
+
 
 And for inspiration:
 + The Ballad of Palindrome <https://www.youtube.com/watch?v=g8vpCSE3ocM>
--- p1/index-view.php
+++ p1/index-view.php
@@ -9,18 +9,36 @@
 
 <body>
     <h1>String Processor</h1>
+
+    <form method='POST' action='process.php'>
+
+        <label for='inputString'>Enter a string:</label>
+        <input type='text' name='inputString' id='inputString' value='<?php echo $inputString ?? "" ?>'>
+
+        <button type='submit'>Process</button>
+    </form>
+
+    <?php if (isset($results)) { ?>
+    <h2>Results</h2>
+    <p>
+        You entered: <?php echo $inputString; ?>
+    </p>
+
     <ul>
-        <li>String: <?php echo $input_string ?>
-        </li>
-        <li>The string has this many vowels : <?php echo $vowel_count ?></li>
+        <li>The string has this many vowels : <?php echo $vowelCount ?></li>
         <li> Is it a pallindrome?
-            <?php if ($is_palindrome) { ?>
+            <?php if ($isPalindrome) { ?>
             Yup
             <?php } else { ?>
             Nope
             <?php } ?>
         </li>
+        <li>Shift the letters: <?php echo $letterShift ?></li>
+        <li>Say it in Pig Latin*: <?php echo $pigLatin ?></li>
     </ul>
+    <cite>*<a href="https://en.wikipedia.org/wiki/Pig_Latin">Igpay Atinlay inyay Ikipediaway</a>
+        <?php } ?>
+
 </body>
 
 </html>
